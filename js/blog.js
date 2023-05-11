4bc386285740c59f25a4b03837cf9543ea408b84
Abhighjna P Chandavarkar <113194114+AbhighjnaP@users.noreply.github.com>
2023-05-11 17:01:16 +0530

    Delete blog.js

--- js/blog.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function() {
-			var circle = document.getElementById("myCircle");
-			if (window.pageYOffset > 100) {
-				circle.classList.add("convex");
-			} else {
-				circle.classList.remove("convex");
-			}
-		}
